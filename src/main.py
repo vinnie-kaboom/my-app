972f9ed452a5f164c824c0a3517e62a4e458a294
vinnie-kaboom <74970846+vinnie-kaboom@users.noreply.github.com>
2026-05-19 08:53:21 -0400

    Change default APP_COLOR from orange to pink

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from datetime import datetime, timezone
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 VERSION = os.environ.get("BUILD_VERSION", "dev")
-APP_COLOR = os.environ.get("APP_COLOR", "orange")
+APP_COLOR = os.environ.get("APP_COLOR", "pink")
 ENVIRONMENT = os.environ.get("ENVIRONMENT", "unknown")
 DEMO_BANNER = os.environ.get("DEMO_BANNER", "UPDATED - PIPELINE02")
 HOSTNAME = socket.gethostname()
